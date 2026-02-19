e0bc2ea0e13de3701dfda377ff40c41385d503e2
mariana castro <marianacastrorc@gmail.com>
2026-02-19 18:10:13 -0300

    adding favicon

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Type Speeding Test</title>
+        <link
+          rel="icon"
+          type="image/svg"
+          href="/assets/images/logo-small.svg"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
